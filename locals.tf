44330690ae7c895c2243980a66c7757747b0651f
Malleus Malegicarum <o@maleficarum.mx>
2025-08-28 10:47:11 -0600

    feat(All): Add support to use a preexisting comparment
    BREAKING CHANGE: Closes #12

--- locals.tf
+++ locals.tf
@@ -2,4 +2,10 @@ locals {
   compute_ip_list = {
     for idc, ip in var.compute_instances_ips : idc => ip
   }
+
+  compartment_id = (
+    var.existing_compartment != "" ? 
+    var.existing_compartment : 
+    (length(oci_identity_compartment.dns_compartment) > 0 ? oci_identity_compartment.dns_compartment[0].id : var.compartment_id)
+  )
 }
